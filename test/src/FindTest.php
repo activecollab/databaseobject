b21c8fc5177d014633a0bb161710d0b7fef85d05
Ilija Studen <ilija.studen@activecollab.com>
2020-01-25 15:07:46 +0100

    Improve code style

--- test/src/FindTest.php
+++ test/src/FindTest.php
@@ -6,6 +6,8 @@
  * (c) A51 doo <info@activecollab.com>. All rights reserved.
  */
 
+declare(strict_types=1);
+
 namespace ActiveCollab\DatabaseObject\Test;
 
 use ActiveCollab\DatabaseConnection\Result\Result;
@@ -17,9 +19,6 @@ use ActiveCollab\DateValue\DateValue;
 use InvalidArgumentException;
 use TypeError;
 
-/**
- * @package ActiveCollab\DatabaseObject\Test
- */
 class FindTest extends WritersTypeTestCase
 {
     public function testCount()
